1c3038848a2e75b1f8a35324b34f204074383a55
Rart3x <ramjattankeny971@gmail.com>
2025-06-23 18:11:08 +0200

    feat: simplified main file

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 import os
-import pygame
 
-
-def snake():
-    """Snake function"""
-    pygame.init()
-    screen = pygame.display.set_mode((800, 600))
-    clock = pygame.time.Clock()
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        clock.tick(60)
-
-    pygame.quit()
+from snake import *
 
 
 def main():

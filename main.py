3170c6b94541708a91d4fc9d0b512430a28bf60a
Rart3x <ramjattankeny971@gmail.com>
2025-09-23 18:23:40 +0200

    feat: better way for flags

--- main.py
+++ main.py
@@ -17,15 +17,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Learn2Slither with training options.")
 
     parser.add_argument("--board", action="store_true", help="Display the board during training.")
-    parser.add_argument("--learn", type=bool, default=True, help="Enable learning during training.")
+    parser.add_argument("--learn", action="store_true", help="Enable learning during training.")
+    parser.add_argument("--visual", action="store_true", help="Enable visual mode.")
+
     parser.add_argument("--load", type=str, default="./models/empty_model.txt",
                 help="Path to save the trained model.")
     parser.add_argument("--save", type=str, default="./models/",
                 help="Path to save the trained model.")
+    
     parser.add_argument("--sessions", type=int, default=0,
                         help="Number of training sessions for the AI model.")
-    parser.add_argument("--visual", type=str, default="ON",
-                    help="Visualize the training process (ON/OFF).")
 
     args = parser.parse_args()
 
